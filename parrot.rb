c4ebc6be2ea60a6c89740f41e5c0b6ca047d2259
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-03-25 06:48:31 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(phrase="Squawk!")
-  puts "I am a parrot"
-  "I am a parrot"
+  puts "my parrot goes #{phrase}"
+  "my parrot goes #{phrase}"
 end
